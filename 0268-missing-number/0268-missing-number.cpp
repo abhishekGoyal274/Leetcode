08977c60c9d1f672ea07d9ee835578e12e497a89
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2023-07-26 23:38:27 +0530

    Time: 99 ms (5.14%), Space: 18 MB (38.91%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int skip = -1;
+        for(int i=0; i<nums.size(); i++){
+            int num = abs(nums[i]);
+            if(num >= nums.size() || (skip != -1 && skip == i)) continue;
+            if(nums[num] == 0){
+                skip = num;
+                nums[0] = -1;
+                nums[num] = -1;
+            }
+            else{
+                nums[num] *= -1; 
+            }
+            // for(int i=0 ;i < nums.size(); i++){
+            //     cout << nums[i] << " ";
+            // }
+            // cout  << endl;
+        }
+        for(int i=0 ;i < nums.size(); i++){
+            cout << nums[i] << " ";
+        }
+        for(int i=0 ;i < nums.size(); i++){
+            if(nums[i] >= 0) return i;
+        }
+        return nums.size();
+    }
+};
