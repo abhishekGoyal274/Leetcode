54bfbe3d9625aa25f57e821272689bdbec699b65
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2023-08-02 13:59:59 +0530

    Time: 5 ms (33.26%), Space: 7.6 MB (60.28%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void helper(vector<int> &nums, int idx, vector<int> &perm, vector<vector<int>> &ans){
+        if(idx >= nums.size()){
+            ans.push_back(perm);
+            return;
+        }
+        
+        int num = nums[idx];
+        for(int i=0; i<nums.size(); i++){
+            if(perm[i] == -20){
+                perm[i] = num;
+                helper(nums,idx+1,perm,ans);
+                perm[i] = -20;
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> perm(nums.size(),-20);
+        helper(nums,0,perm,ans);
+        return ans;
+    }
+};
