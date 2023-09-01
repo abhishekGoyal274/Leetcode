32d3e1d3d626b0350fc92d590e4962c667acda3e
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2023-09-01 14:28:43 +0530

    Time: 6 ms (56.68%), Space: 7.9 MB (65.80%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int setBit(int i){
-        int count{0};
-        while(i>0){
-            if(i & 1) count++;
-            i/=2;
-        }
-        return count;
-    }
     vector<int> countBits(int n) {
         vector<int> ans(n+1,0);
-        for(int i=0; i<=n; i++){
-            ans[i] = setBit(i);
+        if(n == 0) return ans;
+        else if(n == 1){
+            ans[1] = 1;
+            return ans;
+        }else if(n == 2){
+            ans[1] = 1;
+            ans[2] = 1;
+            return ans;
+        }
+        ans[1] = ans[2] = 1;
+        int powerOftwo{2};
+        for(int i=3; i<=n; i++){
+            if((i&(i-1)) == 0){
+                ans[i] = 1;
+                powerOftwo = i;
+            }
+            else ans[i] = ans[powerOftwo] + ans[i-powerOftwo];
         }
         return ans;        
     }
