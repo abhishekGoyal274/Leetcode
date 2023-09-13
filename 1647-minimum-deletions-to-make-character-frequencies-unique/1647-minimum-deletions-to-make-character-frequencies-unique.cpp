5f25ca8590856a3dffbe7f6ed557b06d152c2f1d
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2023-09-13 19:41:12 +0530

    Time: 51 ms (75.08%), Space: 17.5 MB (22.57%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        // Create a vector to store the frequency of each lowercase English letter (fixed size of 26).
-        vector<int> freq(26, 0);
-
-        // Iterate through the characters of the input string s to count their frequencies.
-        for (char c : s) {
-            freq[c - 'a']++; // Increment the corresponding frequency counter.
-        }
-
-        // Sort the frequency vector in non-decreasing order.
+        vector<int> freq(26,0);
+        
+        for(auto ch: s) freq[ch-'a']++;
         sort(freq.begin(), freq.end());
-
-        // Initialize a variable to keep track of the minimum number of deletions needed.
-        int del = 0;
-
-        // Iterate through the sorted frequency vector in reverse order.
-        for (int i = 24; i >= 0; i--) {
-            // If the current frequency is 0, break the loop (no more characters with this frequency).
-            if (freq[i] == 0) {
-                break;
-            }
             
-            // Check if the current frequency is greater than or equal to the next frequency.
-            if (freq[i] >= freq[i + 1]) {
-                int prev = freq[i];
-                // Reduce the current frequency to make it one less than the next frequency.
-                freq[i] = max(0, freq[i + 1] - 1);
-                // Update the deletion count by the difference between previous and current frequency.
-                del += prev - freq[i];
+        int ans{0};
+        for(int i=0; i<25; i++){
+            if(freq[i] != freq[i+1] || freq[i] == 0) continue;
+            
+            int newVal{0};
+            for(int j=i; j>=0; j--){
+                if(j==0) {
+                    if(freq[0] != 0) newVal = freq[0] -1;
+                    break;
+                }
+                else if(freq[j] - freq[j-1] > 1){
+                    newVal = freq[j]-1;
+                    break;
+                }
             }
+            ans += freq[i] - newVal;
+            freq[i] = newVal;
+            sort(freq.begin(), freq.end());
+            
         }
-
-        // Return the minimum number of deletions required to make the string "good."
-        return del;
+        return ans;
     }
-};
+}; 
