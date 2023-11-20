08ae1c6a8d1136d91055041fb6f80f22e693c9f5
Abhishek Goyal <51013750+abhishekGoyal274@users.noreply.github.com>
2023-11-20 22:29:14 +0530

    Time: 63 ms (19.59%), Space: 24.9 MB (38.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,15 +3,15 @@ public:
     int longestPalin (string &a, string &b, int n1, int n2 ,vector<vector<int>> &dp) {
         if(n1 >= a.length() || n2 >= b.length()) return 0;
 
-        if(dp[n1][n2] == -1){
-            if(a[n1] == b[n2]) dp[n1][n2] = 1 + longestPalin (a,b, n1+1,n2+1,dp);
-            else{
-                int x = longestPalin(a,b,n1+1,n2,dp);
-                int y = longestPalin(a,b,n1,n2+1,dp);
-                dp[n1][n2] = max(x,y);
-            }
-        }
-        return dp[n1][n2];
+        if(dp[n1][n2] != -1) return dp[n1][n2];
+        if(a[n1] == b[n2]) return dp[n1][n2] = 1 + longestPalin (a,b, n1+1,n2+1,dp);
+        
+        int x = longestPalin(a,b,n1+1,n2,dp);
+        int y = longestPalin(a,b,n1,n2+1,dp);
+        return dp[n1][n2] = max(x,y);
+
+        
+        
     }
 
     int longestCommonSubsequence(string a, string b) {
