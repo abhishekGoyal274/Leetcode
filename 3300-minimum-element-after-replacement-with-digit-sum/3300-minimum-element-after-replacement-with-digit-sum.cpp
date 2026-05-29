e1cac8ac480d9e9ea410daf20e39f5b6856f94b6
Abhishek Goyal <51013750+abhishekGoyal274@users.noreply.github.com>
2026-05-29 15:15:28 +0530

    Time: 0 ms (100%), Space: 27.7 MB (49.3%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int minElement(vector<int>& nums) {
-        int n = nums.size(); int ans{INT_MAX};
-        for(int i=0; i<n; i++){
-            int num = nums[i]; int k{0};
-            while(num){
-                k += num%10;
-                num = num/10;
+        int ans{INT_MAX};
+        for(int i=0; i<nums.size(); i++){
+            int k{0};
+            while(nums[i]){
+                k += nums[i]%10;
+                nums[i] = nums[i]/10;
             }
             ans = min(ans,k);
         }
